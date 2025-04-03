da1306a3f71c3f1de02aee84a3015c4223ad074d
marek4822 <marekbas121@gmail.com>
2025-04-03 21:21:07 +0200

    minor changes

--- start_app.sh
+++ start_app.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd "$WHEN_F1_DIR/backend" || exit 1
-gunicorn -w 4 -b 127.0.0.1:5000 app:app &
+gunicorn -w 4 -b 0.0.0.0:5000 app:app &
 
 cd "$WHEN_F1_DIR/frontend" || exit 1
 serve -s build -l 3000 &
